d816d7b3743eba419899f7a874008cdc136b7d5e
thiago candido <thikan@gmail.com>
2015-02-19 22:02:14 -0200

    adding some seed functions and changed commander args

--- conway.c
+++ conway.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define GRID_SIZE 10
+#define GRID_SIZE 20
+#define MID GRID_SIZE / 2
 
 enum game_config {
 	glider = 1,
@@ -10,52 +11,58 @@ enum game_config {
 	ten_cell_row,
 	lw_spaceship,
 	tumbler,
-	gosper_glider_gun
+	gosper_glider_gun,
+	not_found
 } typedef g_config;
 
 g_config handle_commands(int,char**);
-void set_seed(int**);
+void set_seed();
 void print_help();
-void set_seed_gosper_glider_gun(int**);
-void set_seed_tumbler(int**);
-void set_seed_lw_spaceship(int**);
-void set_seed_ten_cell_row(int**);
-void set_seed_exploder(int**);
-void set_seed_small_exploder(int**);
-void set_seed_glider(int**);
-void play(int**);
+void set_seed_gosper_glider_gun();
+void set_seed_tumbler();
+void set_seed_lw_spaceship();
+void set_seed_ten_cell_row();
+void set_seed_exploder();
+void set_seed_small_exploder();
+void set_seed_glider();
+void set_from_offset(int a,int b);
+void play();
+
+int grid[GRID_SIZE][GRID_SIZE]  = { 0 };
 
 int main(int argc,char** argv){
-	int grid[GRID_SIZE][GRID_SIZE] = { 0 };
-
+	
 	g_config config = handle_commands(argc,argv);
 
 	switch (config){
 		case (glider):
-			set_seed_glider(grid);
+			set_seed_glider();
 			break;
 		case (small_exploder):
-			set_seed_small_exploder(grid);
+			set_seed_small_exploder();
 			break;
 		case (exploder):
-			set_seed_exploder(grid);
+			set_seed_exploder();
 			break;
 		case (ten_cell_row):
-			set_seed_ten_cell_row(grid);
+			set_seed_ten_cell_row();
 			break;
 		case (lw_spaceship):
-			set_seed_lw_spaceship(grid);
+			set_seed_lw_spaceship();
 			break;
 		case (tumbler):
-			set_seed_tumbler(grid);
+			set_seed_tumbler();
 			break;
 		case (gosper_glider_gun):
-			set_seed_gosper_glider_gun(grid);
+			set_seed_gosper_glider_gun();
+			break;
+		default:
+			printf("config not found!\n");
+			exit(EXIT_FAILURE);
 			break;
 	}
 
-	play(grid);
-	printf("success \n");
+	play();
 	return EXIT_SUCCESS;
 }
 
@@ -72,49 +79,113 @@ g_config handle_commands(int argc, char** argv){
 		print_help();
 		exit(EXIT_SUCCESS);
 	} else {
-		return command_string[0];
+		if (strcmp(command_string,"gli"))
+			return glider;
+		else if (strcmp(command_string,"sex"))
+			return small_exploder;
+		else if (strcmp(command_string,"ex"))
+			return exploder;
+		else if (strcmp(command_string,"tcr"))
+			return ten_cell_row;
+		else if (strcmp(command_string,"lws"))
+			return lw_spaceship;
+		else if (strcmp(command_string,"tbl"))
+			return tumbler;
+		else if (strcmp(command_string,"ggg"))
+			return gosper_glider_gun;
 	}
+
+	return not_found;
 }
 
 void print_help(){
 	printf("\n\t commands:\n");
-	printf("\t\t1 for glider\n");
-	printf("\t\t2 for small exploder\n");
-	printf("\t\t3 for exploder\n");
-	printf("\t\t4 for ten cell row\n");
-	printf("\t\t5 for lightweight spaceship\n");
-	printf("\t\t6 for tumbler\n");
-	printf("\t\t7 for gospel glider gun\n\n");
+	printf("\t\tgli for glider\n");
+	printf("\t\tsex for small exploder\n");
+	printf("\t\tex for exploder\n");
+	printf("\t\ttcr for ten cell row\n");
+	printf("\t\tlws for lightweight spaceship\n");
+	printf("\t\ttbl for tumbler\n");
+	printf("\t\tggg for gospel glider gun\n\n");
 }
 
-void set_seed_gosper_glider_gun(int** grid){
-
-}
-
-void set_seed_tumbler(int** grid){
+void set_seed_gosper_glider_gun(){
 
 }
 
-void set_seed_lw_spaceship(int** grid){
+void set_seed_tumbler(){
 
 }
 
-void set_seed_ten_cell_row(int** grid){
-
+void set_seed_lw_spaceship(){
+	set_from_offset(1,-2);
+	set_from_offset(1,1);
+	set_from_offset(0,2);
+	set_from_offset(-1,2);
+	set_from_offset(-2,2);
+	set_from_offset(-2,1);
+	set_from_offset(-2,0);
+	set_from_offset(-2,-1);
+	set_from_offset(-1,-2);
 }
 
-void set_seed_exploder(int** grid){
-
+void set_seed_ten_cell_row(){
+	set_from_offset(0,0);
+	set_from_offset(0,1);
+	set_from_offset(0,-1);
+	set_from_offset(0,-2);
+	set_from_offset(0,2);
+	set_from_offset(0,3);
+	set_from_offset(0,-3);
+	set_from_offset(0,4);
+	set_from_offset(0,-4);
+	set_from_offset(0,5);
 }
 
-void set_seed_small_exploder(int** grid){
-
+void set_seed_exploder(){
+	set_from_offset(-2,-2);
+	set_from_offset(-1,-2);
+	set_from_offset(0,-2);
+	set_from_offset(1,-2);
+	set_from_offset(2,-2);
+	set_from_offset(-2,2);
+	set_from_offset(-1,2);
+	set_from_offset(0,2);
+	set_from_offset(1,2);
+	set_from_offset(2,2);
+	set_from_offset(-2,0);
+	set_from_offset(2,0);
 }
 
-void set_seed_glider(int** grid){
-
+void set_seed_small_exploder(){
+	set_from_offset(0,0);
+	set_from_offset(0,-1);
+	set_from_offset(0,1);
+	set_from_offset(1,1);
+	set_from_offset(1,-1);
+	set_from_offset(2,0);
+	set_from_offset(-1,0);
 }
 
-void play(int** grid){
+void set_seed_glider(){
+	set_from_offset(0,0);
+	set_from_offset(0,-1);
+	set_from_offset(0,1);
+	set_from_offset(-1,1);
+	set_from_offset(-2,0);
+}
 
+void play(){
+	for (int i = 0; i < GRID_SIZE; ++i)
+	{
+		for (int j = 0; j < GRID_SIZE; ++j)
+		{
+			printf("%2d",grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void set_from_offset(int a, int b){
+	grid[MID+a][MID+b] = 1;
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-gcc conway.c -o conway -std=c99 
+gcc conway.c -o conway -std=c99 on gcc 4.9.1
